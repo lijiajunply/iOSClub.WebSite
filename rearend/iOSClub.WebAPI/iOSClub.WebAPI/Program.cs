c7ffff69ddb221991350ee72a475ed6dc8799457
李嘉俊 <2383690354@qq.com>
2025-09-12 01:04:04 +0800

    试一下在生产环境中会不会出现http问题

--- rearend/iOSClub.WebAPI/iOSClub.WebAPI/Program.cs
+++ rearend/iOSClub.WebAPI/iOSClub.WebAPI/Program.cs
@@ -102,11 +102,15 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
 //     
 // }
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
 
 app.MapOpenApi();
 
