a100e5a90cc4be988e24cce309761fea4d75b933
LuckyFish at macOS <2383690354@qq.com>
2025-11-01 03:17:07 +0800

    feat(migration): 添加学生表邮箱和密码字段并优化数据类型
    -为 Students 表新增 EMail 字段，类型为 varchar(256)
    -为 Students 表新增 PasswordHash 字段，类型为 varchar(256)
    - 将所有表中的字段类型从 varchar 调整为 TEXT，并设置最大长度限制
    - 添加 Todos 表的 CreatedTime 字段，默认值为未指定时间
    - 更新迁移文件以反映新的数据库结构和字段约束

---
+++ rearend/iOSClub.WebAPI/iOSClub.Data/Migrations/20250808152029_AddIdentity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iOSClub.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EMail",
+                table: "Students",
+                type: "varchar(256)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Students",
+                type: "varchar(256)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EMail",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Students");
+        }
+    }
+}
